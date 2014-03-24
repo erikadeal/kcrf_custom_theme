a33cb99e98ccfab42273e4497dbc1f89f803c9dc
erikadeal <erika.deal@gmail.com>
2014-03-24 12:36:09 -0700

    Better user loop on authors.php, plus a little styling

--- author.php
+++ author.php
@@ -43,46 +43,13 @@
 					<div class="sidebar fourcol last clearfix" role="complimentary">
 
 						<h4>All Members</h4>
+						
 							<?php
-								$display_admins = false;
-								$order_by = 'display_name'; // 'nicename', 'email', 'url', 'registered', 'display_name', or 'post_count'
-								$role = 'forum_member'; // 'subscriber', 'contributor', 'editor', 'author' - leave blank for 'all'
-								$hide_empty = false; // hides authors with zero posts
-
-								if(!empty($display_admins)) {
-									$blogusers = get_users('orderby='.$order_by.'&role='.$role);
-								} 
-
-								else {
-									$admins = get_users('role=administrator');
-									$exclude = array();
-									foreach($admins as $ad) {
-										$exclude[] = $ad->ID;
-									}
-									$exclude = implode(',', $exclude);
-									$blogusers = get_users('exclude='.$exclude.'&orderby='.$order_by.'&role='.$role);
-								}
-
-								$authors = array();
-
-								foreach ($blogusers as $bloguser) {
-									$user = get_userdata($bloguser->ID);
-									if(!empty($hide_empty)) {
-										$numposts = count_user_posts($user->ID);
-										if($numposts < 1) continue;
-									}
-									$authors[] = (array) $user;
-								}
-
-								echo '<ul class="contributors">';
-								foreach($authors as $author) {
-									$display_name = $author['data']->display_name;
-									$author_profile_url = get_author_posts_url($author['ID']);
-
-									echo '<li><a href="', $author_profile_url, '">', $display_name, '</a></li>';
-								}
-								echo '</ul>';
-								?>
+							    $blogusers = get_users('role=forum_member');
+							    foreach ($blogusers as $user) {
+							        echo '<p><a href="' . home_url() . '/?author=' . $user->ID . '">' . $user->organization_name . '</a></p>';
+							    }
+							?>
 
 						</div>
 					</div>
